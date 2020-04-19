b2fb7a9bcda1165b04e16efeeb4c14700972475b
freek <freek@spatie.be>
2020-04-19 23:05:03 +0200

    wip

--- src/Commands/ListenForMentionsCommand.php
+++ src/Commands/ListenForMentionsCommand.php
@@ -28,7 +28,7 @@ class ListenForMentionsCommand extends Command
 
         (new PublicStream(
             $configuration['access_token'],
-            $configuration['access_secret'],
+            $configuration['access_token_secret'],
             $configuration['consumer_key'],
             $configuration['consumer_secret'],
         ))
