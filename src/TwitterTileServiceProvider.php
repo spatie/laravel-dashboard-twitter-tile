d37bb69f26a382ff35f37af8e0ca5357f1826ae0
freek <freek@spatie.be>
2020-04-21 15:35:40 +0200

    wip

--- src/TwitterTileServiceProvider.php
+++ src/TwitterTileServiceProvider.php
@@ -20,6 +20,10 @@ class TwitterTileServiceProvider extends ServiceProvider
             ]);
         }
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashboard-twitter-tile-views');
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/dashboard-twitter-tile'),
+        ], 'dashboard-calendar-twitter-views');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashboard-twitter-tile');
     }
 }
